d1557ebf5de852553ba241c403c9981e1691975d
Erickng7 <signmega065@gmail.com>
2022-12-09 04:42:06 -0800

    4. Hello, puts-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #!include <studio.h>
-
 /**
- *  main - prints a given phrase
- *  Return: 0 if exited properly, non-zero otherwise
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 
-int  main(void)
-
+int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puta("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
