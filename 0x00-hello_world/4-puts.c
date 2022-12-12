9e11c78f79489e39d1f624089361cb8c7e5dcbe9
Erickng7 <signmega065@gmail.com>
2022-12-11 20:47:22 -0800

    Task 4 original

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#!include <stdio.h>
+#include <stdio.h>
+
 /*
- * main - print a line of code using puts
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+
+}       	
