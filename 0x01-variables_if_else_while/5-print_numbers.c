7dc777d96614e493bcd48a594e63e74b39915906
Erickng7 <signmega065@gmail.com>
2022-12-22 00:20:01 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - print all single digits number of base 10
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+	int var;
+	
+	for (var = 0; var < 10; var++)
+		printf("%d", var);
+	
+	printf("\n");
+	
+	return (0);
+
+
+
+}
